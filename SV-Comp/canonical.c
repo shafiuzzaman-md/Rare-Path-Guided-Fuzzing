5f6920af05ed585bfacab7b76db2eb13f37a9d9d
Md Shafiuzzaman <md.shafiuzzaman.hira@gmail.com>
2023-01-22 11:25:26 -0800

    Canonical example

---
+++ SV-Comp/canonical.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv)
+{
+    char ptr[20];
+    int tmp = 0;
+    FILE *fp = fopen(argv[1], "r");
+    fgets(ptr, sizeof(ptr), fp);
+
+   if((int)(ptr[0] == 65)){
+     tmp = 1;
+     printf("%d",ptr[0]);
+   }
+
+   if((int)(ptr[1] == 66)){
+     tmp = 2;
+     printf("%d",ptr[0]);
+   }
+
+   if((int)(ptr[2] == 67)){
+     tmp = 3;
+     printf("%d",ptr[0]);
+   }
+        
+
+    if (!tmp)
+    {
+        abort();
+    }
+    return 0;
+}
